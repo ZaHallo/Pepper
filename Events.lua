c2c9154969f2f5be61f4782727aef0328c74ab23
ZaHallo <95763671+ZaHallo@users.noreply.github.com>
2021-12-08 14:11:49 -0500

    Add files via upload

---
+++ Events.lua
@@ -0,0 +1,41 @@
+Pepper.Signals = {}
+Pepper.Signals.eventmeta = {}
+Pepper.Signals.connectionmeta = {}
+
+--Event
+function Pepper.Signals.Event()
+	local this = setmetatable({}, {__invoke = Pepper.Signals.eventmeta})
+
+	this.Connections = {}
+
+	return this
+end
+
+function Pepper.Signals.eventmeta:Invoke()
+	for Connection in pairs(self.Connections) do
+		Connection.Lambda()
+	end
+end
+
+function Pepper.Signals.eventmeta:Connect(ToCall)
+	local NewConnection = Pepper.Signals.Connection(self,ToCall)
+	this.Connections[NewConnection] = NewConnection
+	return NewConnection
+end
+
+--Connection
+function Pepper.Signals.Connection(Event, ToCall)
+	local this = setmetatable({}, {__invoke = Pepper.Signals.connectionmeta})
+
+	this.ConnectedEvent = Event
+	this.Lambda = ToCall
+	this.Active = true
+
+	return this
+end
+
+function Pepper.Signals.connectionmeta:Disconnect()
+	self.ConnectedEvent.Connections[self] = nil
+	self.ConnectedEvent = nil
+	self.Active = false
+end
