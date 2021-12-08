2e8a6aa24e26dc8fea37d3e254fb8d6c2047de97
ZaHallo <95763671+ZaHallo@users.noreply.github.com>
2021-12-08 10:22:43 -0500

    dsads
    sadasdsa

---
+++ console.lua
@@ -0,0 +1,26 @@
+local console = require("Pepper.internal.console")
+
+local main = {}
+
+function main.file_exists(name)
+  local f=io.open(name,"r")
+  if f~=nil then 
+	 	io.close(f)	
+		return true 
+	else 
+		return false 
+	end
+end
+
+function main.get_package(packagepath, saveasdirectory)
+	console.console("curl -o "..saveasdirectory.." "..packagepath)
+	console.console("clear")
+end
+
+function  main.tryGet(packagepath,saveasdirectory)
+	if not main.file_exists(saveasdirectory) then
+		main.get_package(packagepath,saveasdirectory)
+	end
+end
+
+return main
