d78c3e7ee956230f6bd7938cd1853cba846ecab7
ZaHallo <95763671+ZaHallo@users.noreply.github.com>
2021-12-08 12:38:00 -0500

    Update console.lua

--- console.lua
+++ console.lua
@@ -8,4 +8,14 @@ function main.consoleReturn(command)
    return io.popen(command):lines("*a")
 end
 
+function main.directory_exists( sPath )
+  if type( sPath ) ~= "string" then return false end
+
+  local response = os.execute( "cd " .. sPath )
+  if response == 0 then
+    return true
+  end
+  return false
+end
+
 return main
